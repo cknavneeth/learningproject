5955ff73acd907fb12eb96ddc2c0acd1df8c7f10
cknavneeth <navaneethck03@gmail.com>
2025-03-29 21:26:03 +0530

    eda sajiyee

--- lmsbackend/backend/src/admin/admin.module.ts
+++ lmsbackend/backend/src/admin/admin.module.ts
@@ -7,9 +7,10 @@ import { user, userSchema } from 'src/users/users.schema';
 import { UsersModule } from 'src/users/users.module';
 import { instructor, instructorSchema } from 'src/instructors/instructor.schema';
 import { InstructorsModule } from 'src/instructors/instructors.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports :[MongooseModule.forFeature([{name:admin.name,schema:adminSchema},{ name: user.name, schema: userSchema },{name:instructor.name,schema:instructorSchema}]),UsersModule, InstructorsModule],
+  imports :[MongooseModule.forFeature([{name:admin.name,schema:adminSchema},{ name: user.name, schema: userSchema },{name:instructor.name,schema:instructorSchema}]),UsersModule, InstructorsModule, CategoryModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports:[AdminService],
